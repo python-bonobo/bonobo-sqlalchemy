7a6a5727a481e2a123bce4edd54eb424606fcfa5
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-24 16:55:06 +0200

    change db pattern

--- bonobo_sqlalchemy/util.py
+++ bonobo_sqlalchemy/util.py
@@ -7,7 +7,17 @@ from bonobo_sqlalchemy.logging import get_logger
 
 logger = get_logger()
 
-POSTGRES_DEFAULTS = {
+# mysql default config
+# DATABASE_CONFIG_DEFAULTS = {
+#     'driver': 'mysql',
+#     'host': 'localhost',
+#     'port': '3306',
+#     'name': 'mysql',
+#     'user': 'root',
+#     'pass': '',
+# }
+
+DATABASE_CONFIG_DEFAULTS = {
     'driver': 'postgres',
     'host': 'localhost',
     'port': '5432',
@@ -19,8 +29,8 @@ POSTGRES_DEFAULTS = {
 DSN_TEMPLATE = '{driver}://{user}:{pass}@{host}:{port}/{name}'
 
 
-def create_postgresql_engine(*, options='client_encoding=utf8', env='POSTGRES', **kwargs):
-    config = defaultdict(**POSTGRES_DEFAULTS)
+def create_postgresql_engine(*, env='POSTGRES', **kwargs):
+    config = defaultdict(**DATABASE_CONFIG_DEFAULTS)
     for var in ('driver', 'user', 'pass', 'host', 'port', 'name'):
         if var in kwargs:
             config[var] = kwargs.pop(var)
@@ -29,8 +39,8 @@ def create_postgresql_engine(*, options='client_encoding=utf8', env='POSTGRES', 
             if env_var in environ:
                 config[var] = environ[env_var]
     dsn = DSN_TEMPLATE.format(**config)
-    if options:
-        dsn += '?' + options
+    if DATABASE_CONFIG_DEFAULTS['driver'].upper() == 'POSTGRES':  # or env == 'POSTGRES'
+        dsn += '?client_encoding=utf8'
 
     logger.info('Creating database engine: ' + dsn)
 
